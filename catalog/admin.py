7404549a8d10e021e9ec0747917ba9e4b1bb0679
saysuu <skysaika@yandex.ru>
2023-12-12 21:16:08 +0300

    create admin and superuser

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from catalog.models import Category, Product
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка модели Категория"""
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админка модели Продукт"""
+    list_display = ['name', 'pk', 'slug', 'price', 'category',
+                    'available', 'created', 'updated']
+    list_filter = ['category', 'available', 'created', 'updated']
+    search_fields = ['name', 'slug', 'description']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
