7e21ff66fbf049e2cb880e0c907b6a38f8ac6ec2
saysuu <skysaika@yandex.ru>
2024-01-21 19:19:58 +0300

    add form_valid with send_mail to verify new_user, email_config insettings

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.urls import reverse_lazy
@@ -24,3 +26,13 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        """Отправка письма с приветствием при регистрации"""
+        new_user = form.save()  # сохраняем пользователя
+        send_mail(
+            subject='Поздравляем с регистрацией!',
+            message='Вы успешно зарегистрировались на аншей платформе.',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email],  #
+        )
+        return super().form_valid(form)
