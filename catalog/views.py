47174d2b5530ec3bc45aa14ca1c1f489f0dec964
saysuu <skysaika@yandex.ru>
2024-02-06 03:23:21 +0300

    low level cache in CategoryListView

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 import random
 
+from django.conf import settings
+from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
@@ -116,6 +118,15 @@ class CategoryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Список категорий'
+        if settings.CACHE_ENABLED:
+            key = 'category_list'
+            category_list = cache.get(key)
+            if category_list is None:
+                category_list = list(Category.objects.all())
+                cache.set(key, category_list, 60)  # Кэширование на 60 секунд
+                return category_list
+            else:
+                return Category.objects.all()
         return context
 
 
